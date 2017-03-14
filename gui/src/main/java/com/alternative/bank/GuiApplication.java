26b67499867e82325a332b870d8078e0752175b4
Yannic Luyckx <yannic.luyckx@gmail.com>
2017-03-13 22:30:07 -0400

    Bootstrap gui with Web,Security,Actuator,Config Client,Eureka Discovery,Hystrix,Sleuth

---
+++ gui/src/main/java/com/alternative/bank/GuiApplication.java
@@ -0,0 +1,12 @@
+package com.alternative.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiApplication.class, args);
+	}
+}
