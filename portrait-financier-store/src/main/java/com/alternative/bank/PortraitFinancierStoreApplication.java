11428e10d93d81eaabf97676881ff8a8e7aa833b
Yannic Luyckx <yannic.luyckx@gmail.com>
2017-03-13 22:36:32 -0400

    Bootstrap portrait-financier-store with Rest Repositories, JPA, H2, Sleuth, Actuator, Security

---
+++ portrait-financier-store/src/main/java/com/alternative/bank/PortraitFinancierStoreApplication.java
@@ -0,0 +1,12 @@
+package com.alternative.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortraitFinancierStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortraitFinancierStoreApplication.class, args);
+	}
+}
