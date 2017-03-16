523d21bd654ee53c5c39f3b9815d24309ef13617
Yannic Luyckx <yannic.luyckx@gmail.com>
2017-03-15 23:08:46 -0400

    Gui from spring web security example

--- gui/src/main/java/com/alternative/bank/gui/GuiApplication.java
+++ gui/src/main/java/com/alternative/bank/gui/GuiApplication.java
@@ -1,4 +1,4 @@
-package com.alternative.bank;
+package com.alternative.bank.gui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
